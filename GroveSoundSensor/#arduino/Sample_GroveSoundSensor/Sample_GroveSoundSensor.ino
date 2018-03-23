4fffbf005eb36bb5a4b69269bf2f51aa963b281f
Teppei Kuroda <niamugi@gmail.com>
2018-03-23 16:53:23 +0900

    GroveSoundSensorのノイズ(？)がひどい ローパスフィルターなどで対策してある程度滑らかにしてみたけど、安定しない。 ついでにProcessingでグラフ表示を試してみた。

--- GroveSoundSensor/#arduino/Sample_GroveSoundSensor/Sample_GroveSoundSensor.ino
+++ GroveSoundSensor/#arduino/Sample_GroveSoundSensor/Sample_GroveSoundSensor.ino
@@ -1,24 +1,47 @@
-// test code for Grove - Sound Sensor
-// loovee @ 2016-8-30
+// Grove SoundSensor
+#define PIN_ADC  A1
 
-const int pinAdc = A1;
+float ave_unit = 0;
+float RC_a = 0.95;  //ローパスフィルタの係数
+float ave_sec = 0;
+float maxAnalogValue = 0;
+int cnt = 0;
+const int MAX_CNT = 50;
+long ave_tmp = 0;
 
 void setup()
 {
-    Serial.begin(115200);
-    Serial.println("Grove - Sound Sensor Test...");
+  Serial.begin(115200);
 }
 
 void loop()
 {
-    long sum = 0;
-    for(int i=0; i<32; i++)
-    {
-        sum += analogRead(pinAdc);
-    }
+  ave_unit = getAnalogValue(ave_unit);
+  if (ave_unit > maxAnalogValue ) maxAnalogValue = ave_unit;
 
-    sum >>= 5;
+  //1テンポ遅れて平均値を出している。
+  ave_tmp += ave_unit;
+  if (++cnt >= MAX_CNT) {
+    ave_sec = ave_tmp / MAX_CNT;
+    cnt = 0;
+    ave_tmp = 0;
+  }
 
-    Serial.println(sum);
-    delay(10);
+  Serial.print(ave_unit);
+  Serial.print(",");
+  Serial.print(ave_sec);
+  Serial.print(",");
+  Serial.print(maxAnalogValue);
+  Serial.println("");
+
+  delay(10);
 }
+
+//RCフィルタ適用
+long getAnalogValue(float y_old) {
+  int x = analogRead(PIN_ADC);
+  float y = RC_a * y_old + (1 - RC_a) * x;
+  return y;
+}
+
+
