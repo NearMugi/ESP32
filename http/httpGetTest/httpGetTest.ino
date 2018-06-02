be1e65b9c813efdd75951834ecd1bcc06bf5d896
Teppei Kuroda <niamugi@gmail.com>
2018-06-02 23:47:11 +0900

    同じLAN上のパソコンへアクセスする方法を模索中 httpClientを使ってみたけどうまくいかない。外部のURLには問題なくアクセスできる

---
+++ http/httpGetTest/httpGetTest.ino
@@ -0,0 +1,34 @@
+#include <Nefry.h>
+#include <HTTPClient.h>
+
+const char URL[] = "http://192.168.0.9:8000";  //"http://unkou.keikyu.co.jp/" なら取得できる
+
+void setup() {
+  String result1 = getPageSource((char*)URL);
+  Serial.println(result1);
+}
+
+void loop() {
+
+}
+
+String getPageSource(char host[]) {
+  Serial.println(host);
+  HTTPClient http;
+
+  http.begin(host);
+  int httpCode = http.GET();
+
+  String result = "";
+
+  if (httpCode < 0) {
+    result = http.errorToString(httpCode);
+  } else if (http.getSize() < 0) {
+    result =  "size is invalid";
+  } else {
+    result = http.getString();
+  }
+
+  http.end();
+  return result;
+}
