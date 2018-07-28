f44c87f4a9df8599c2dad4f6678c38aa85557ae9
Teppei Kuroda <niamugi@gmail.com>
2018-07-28 14:10:50 +0900

    シールドに合わせたデジタル(アナログ)入力のサンプルソースを追加

---
+++ digitalSW_analogSW/digitalSW_analogSW.ino
@@ -0,0 +1,34 @@
+#include <Nefry.h>
+#include <NefryDisplay.h>
+#include <NefrySetting.h>
+void setting() {
+  Nefry.disableDisplayStatus();
+  Nefry.disableWifi();
+}
+NefrySetting nefrySetting(setting);
+
+#define PIN_DIGITAL_SW D8
+#define PIN_ANALOG_SW A3
+
+
+void setup() {
+  NefryDisplay.begin();
+  NefryDisplay.setAutoScrollFlg(true);//自動スクロールを有効
+
+  NefryDisplay.clear();
+  NefryDisplay.display();
+
+  // put your setup code here, to run once:
+  pinMode(PIN_DIGITAL_SW, INPUT);
+  pinMode(PIN_ANALOG_SW, INPUT);
+
+}
+
+void loop() {
+  NefryDisplay.clear();
+  NefryDisplay.setFont(ArialMT_Plain_16);
+  NefryDisplay.drawString(0, 0, String(digitalRead(PIN_DIGITAL_SW)));
+  NefryDisplay.drawString(0, 20, String(analogRead(PIN_ANALOG_SW)));
+  NefryDisplay.display();
+  Nefry.ndelay(20);
+}
