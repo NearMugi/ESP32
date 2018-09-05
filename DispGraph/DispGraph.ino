d1fe772fbc592e1ea9d89669025160aff361ee19
Teppei Kuroda <niamugi@gmail.com>
2018-09-05 17:57:59 +0900

    折れ線グラフのクラス作成 3つまで同時表示できる。 1秒ごとに垂線を引く。

--- DispGraph/DispGraph.ino
+++ DispGraph/DispGraph.ino
@@ -10,34 +10,40 @@ NefrySetting nefrySetting(setting);
 //ループ周期(us)
 #include <interval.h>
 
+//グラフ一時停止
+bool isStopGraph = false;
+
 //サンプルデータ
-long sampleData;
+long sampleData[3];
+float deg = 0;
 
-void setup() {
-  NefryDisplay.begin();
-  NefryDisplay.setAutoScrollFlg(true);//自動スクロールを有効
-
-  NefryDisplay.clear();
-  NefryDisplay.display();
-
-  dispGraph_init();
-
-  //サンプルデータ生成用乱数シード
-  randomSeed(analogRead(0));
-}
 
 //グラフ
 #include "dispGraph.h"
+//static変数を定義
+int graph_line::lpTime;
+int graph_line::posX;
+int graph_line::posY;
+int graph_line::lenX;
+int graph_line::lenY;
+int graph_line::dpp;
+int graph_line::valueMIN;
+int graph_line::valueMAX;
+int graph_line::valueSIZE;
+
+//折れ線グラフの領域
 #define LOOPTIME_GRAPH 100000
 #define GRAPH_POS_X 25
 #define GRAPH_POS_Y 10
 #define GRAPH_LEN_X 100
 #define GRAPH_LEN_Y 50
-#define GRAPH_DPP 5 //点をプロットする間隔(1なら1ドットにつき1点、2なら2ドットにつき1点)
+#define GRAPH_DPP 10 //点をプロットする間隔(1なら1ドットにつき1点、2なら2ドットにつき1点)
 
 #define VALUE_MIN 0
 #define VALUE_MAX 1023
 #define VALUE_SIZE (GRAPH_LEN_X / GRAPH_DPP) + 1
+int x[VALUE_SIZE];  //x座標
+int t[VALUE_SIZE];  //一定間隔に垂線を引くための配列(垂線の有無)
 graph_line grline = graph_line(
                       LOOPTIME_GRAPH,
                       GRAPH_POS_X,
@@ -47,73 +53,78 @@ graph_line grline = graph_line(
                       GRAPH_DPP,
                       VALUE_MIN,
                       VALUE_MAX,
-                      VALUE_SIZE
+                      VALUE_SIZE,
+                      &x[0],
+                      &t[0]
                     );
 
-int p[VALUE_SIZE][2];//グラフでプロットするx座標,データ(グラフ用に変換前)
-int vMax[2];  //最大値,グラフでプロットする位置
+//折れ線グラフのグラフの設定
+//頂点の数が可変なので外部で配列を用意している
+#define GL_1 0
+#define GL_2 1
+#define GL_3 2
+int v1[VALUE_SIZE]; //頂点の値
+int p1[VALUE_SIZE]; //グラフでプロットするy座標
+int v2[VALUE_SIZE]; //頂点の値
+int p2[VALUE_SIZE]; //グラフでプロットするy座標
+int v3[VALUE_SIZE]; //頂点の値
+int p3[VALUE_SIZE]; //グラフでプロットするy座標
 
-void dispGraph_init() {
-  for (int i = 0; i < VALUE_SIZE; i++) {
-    p[i][0] = GRAPH_POS_X + i * GRAPH_DPP;
-    p[i][1] = 0;
-  }
+void setup() {
+  Nefry.enableSW();
+
+  NefryDisplay.begin();
+  NefryDisplay.setAutoScrollFlg(true);//自動スクロールを有効
+
+  NefryDisplay.clear();
+  NefryDisplay.display();
+
+  //表示するグラフの設定
+  grline.setGraph(GL_1, &v1[0], &p1[0], VERTEX_NONE, DISP_MAX);
+  grline.setGraph(GL_2, &v2[0], &p2[0], VERTEX_CIR, NOTDISP_MAX);
+  grline.setGraph(GL_3, &v3[0], &p3[0], VERTEX_SQU, NOTDISP_MAX);
+
+  //サンプルデータ生成用乱数シード
+  randomSeed(analogRead(0));
 }
 
-void dispGraph_set(int _v) {
-  vMax[0] = _v;  //最大値に仮設定
-  //前にずらす
-  for (int i = 0; i < VALUE_SIZE; i++) {
-    p[i][1] = p[i + 1][1];
-    if (p[i + 1][1] > vMax[0]) vMax[0] = p[i + 1][1];
-  }
-  //最後尾に追加する
-  p[VALUE_SIZE - 1][1] = _v;
-  vMax[1] = map(vMax[0], VALUE_MIN, VALUE_MAX, GRAPH_LEN_Y + GRAPH_POS_Y, GRAPH_POS_Y);
-}
-
-void dispGraph_update() {
+void dispGraphLine_update() {
   //領域の描画
   grline.dispArea();
-
-  //最大値
-  NefryDisplay.drawHorizontalLine(GRAPH_POS_X, vMax[1], GRAPH_LEN_X);
-  NefryDisplay.setFont(ArialMT_Plain_10);
-  NefryDisplay.drawString(GRAPH_POS_X - 20, vMax[1] - 8, String(vMax[0]));
-
-  int p1;
-  int p2;
-  for (int i = 0; i < VALUE_SIZE - 1; i++) {
-    //グラフ向けの値に変換
-    p1 = map(p[i][1], VALUE_MIN, VALUE_MAX, GRAPH_LEN_Y + GRAPH_POS_Y, GRAPH_POS_Y);
-    p2 = map(p[i + 1][1], VALUE_MIN, VALUE_MAX, GRAPH_LEN_Y + GRAPH_POS_Y, GRAPH_POS_Y);
-
-    NefryDisplay.drawLine(p[i][0], p1, p[i + 1][0], p2);  //各座標を線で結ぶ
-    //各座標をプロットする
-    NefryDisplay.fillCircle(p[i][0], p1, 2); //頂点を丸くする
-    //NefryDisplay.fillRect(p[i][0] - 2, p1 - 2, 4, 4); //頂点を四角くする
-  }
-  //最後の点をプロット
-  p1 = map(p[VALUE_SIZE - 1][1], VALUE_MIN, VALUE_MAX, GRAPH_LEN_Y + GRAPH_POS_Y, GRAPH_POS_Y);
-  NefryDisplay.fillCircle(p[VALUE_SIZE - 1][0], p1, 2); //頂点を丸くする
-  //NefryDisplay.fillRect(p[VALUE_SIZE - 1][0] - 2, p1 - 2, 4, 4); //頂点を四角くする
-
+  grline.updateGraph();
 }
 
 void DispNefryDisplay() {
   NefryDisplay.clear();
   //取得したデータをディスプレイに表示
   NefryDisplay.setFont(ArialMT_Plain_10);
-  NefryDisplay.drawString(105, 0, String(sampleData));
-  dispGraph_update();
+  NefryDisplay.drawString(35, 0, String(sampleData[0]));
+  NefryDisplay.drawString(70, 0, String(sampleData[1]));
+  NefryDisplay.drawString(105, 0, String(sampleData[2]));
+  dispGraphLine_update();
   NefryDisplay.display();
 }
 
 
 void loop() {
+  if (Nefry.readSW()) {
+    isStopGraph = !isStopGraph;
+  }
+
+
   interval<LOOPTIME_GRAPH>::run([] {
-    sampleData = random(VALUE_MIN, VALUE_MAX + 1 - 300);
-    dispGraph_set((int)sampleData);
+    if (!isStopGraph) {
+      sampleData[0] = random(VALUE_MIN + 300, VALUE_MAX + 1 - 300);
+      sampleData[1] = (VALUE_MAX / 2) * (1 + sin(deg / (180 / PI)));
+      sampleData[2] = (VALUE_MAX / 2) * (1 + cos(deg / (180 / PI)));
+      deg += 10;
+
+      grline.addGraphData(GL_1, (int)sampleData[0]);
+      grline.addGraphData(GL_2, (int)sampleData[1]);
+      grline.addGraphData(GL_3, (int)sampleData[2]);
+      grline.updateGraphTime();
+    }
+
     DispNefryDisplay();
   });
 }
--- DispGraph/dispGraph.h
+++ DispGraph/dispGraph.h
@@ -3,27 +3,24 @@
 #include <NefryDisplay.h>
 
 //頂点タイプ
-#define VERTEX_NONE 0  //頂点そのまま
-#define VERTEX_CIR 1  //丸
-#define VERTEX_SQU 2  //四角
+enum VERTEX {
+  VERTEX_NONE, //頂点そのまま
+  VERTEX_CIR,  //丸
+  VERTEX_SQU,  //四角
+};
+
+//最大値の表示有無
+#define DISP_MAX true
+#define NOTDISP_MAX false
+
+//表示するグラフ数
+#define GRAPH_CNT 3
 
 //折れ線グラフ
 class graph_line {
 
   public:
-    //グラフ領域・ピッチ・更新時間
-    int lpTime;
-    int posX;
-    int posY;
-    int lenX;
-    int lenY;
-    int dpp;//点をプロットする間隔(1なら1ドットにつき1点、2なら2ドットにつき1点)
-    int valueMIN;
-    int valueMAX;
-    int valueSIZE;
-
-    //コンストラクタ
-    graph_line(int _lpTime, int _posX, int _posY, int _lenX, int _lenY, int _dpp, int _valueMIN, int _valueMAX, int _valueSIZE) {
+    graph_line(int _lpTime, int _posX, int _posY, int _lenX, int _lenY, int _dpp, int _valueMIN, int _valueMAX, int _valueSIZE, int *_x, int *_t) {
       lpTime = _lpTime;
       posX = _posX;
       posY = _posY;
@@ -33,8 +30,21 @@ class graph_line {
       valueMIN = _valueMIN;
       valueMAX = _valueMAX;
       valueSIZE = _valueSIZE;
-    }
 
+      //x座標はこれ以降更新しない。
+      x = _x;
+      for (int i = 0; i < valueSIZE; i++) {
+        *(x + i) = posX + i * dpp;
+      }
+
+      //時間
+      tCntMax = 1000000 / lpTime; //1秒ごとに垂線を引く
+      t = _t;
+      for (int i = 0; i < valueSIZE; i++) {
+        *(t + i) = 0;
+      }
+
+    }
 
     void dispArea() {
       //補助線
@@ -43,13 +53,139 @@ class graph_line {
       NefryDisplay.setFont(ArialMT_Plain_10);
       NefryDisplay.drawString(posX - 20, posY - 8, String(valueMAX));
       NefryDisplay.drawString(posX - 20, posY + lenY - 8, String(valueMIN));
+
+      //時間
+      for (int i = 0; i < valueSIZE; i++) {
+        if (*(t + i) > 0) {
+          NefryDisplay.drawVerticalLine(*(x + i), posY, lenY);
+        }
+      }
+    }
+
+    //グラフクラスの初期化
+    void setGraph(int _idx, int *_v, int *_p, VERTEX _type, bool _isDispMax) {
+      _g[_idx] = graph(&_v[0], &_p[0], _type, _isDispMax);
+      _g[_idx].init();
+    }
+
+    //グラフデータの追加
+    void addGraphData(int _idx, int _v) {
+      if (!_g[_idx].isSet) return;
+      _g[_idx].addData(_v);
+    }
+
+    //時間の更新
+    void updateGraphTime() {
+      bool isSplit = false;
+      if (++tCnt >= tCntMax) {
+        tCnt = 0;
+        isSplit = true;
+      }
+
+      //前にずらす
+      for (int i = 0; i < valueSIZE - 1; i++) {
+        *(t + i) = *(t + i + 1);
+      }
+      //最後尾に追加する
+      *(t + valueSIZE - 1) = (int)isSplit;
+    }
+
+    //グラフデータの描画
+    void updateGraph() {
+      for (int _idx = 0; _idx < GRAPH_CNT; _idx++) {
+        if (!_g[_idx].isSet) continue;
+
+        //最大値
+        if (_g[_idx].isDispMax) {
+          NefryDisplay.drawHorizontalLine(posX, _g[_idx].vMax[1], lenX);
+          NefryDisplay.setFont(ArialMT_Plain_10);
+          NefryDisplay.drawString(posX - 20, _g[_idx].vMax[1] - 8, String(_g[_idx].vMax[0]));
+        }
+
+        for (int i = 0; i < valueSIZE - 1; i++) {
+
+          NefryDisplay.drawLine(*(x + i), *(_g[_idx].p + i),
+                                *(x + i + 1), *(_g[_idx].p + i + 1)); //各座標を線で結ぶ
+          //各座標をプロットする
+          vertexPlot(_g[_idx].type, *(x + i), *(_g[_idx].p + i));
+        }
+        //最後の点をプロット
+        vertexPlot(_g[_idx].type, *(x + valueSIZE - 1), *(_g[_idx].p + valueSIZE - 1));
+      }
+    }
+    void vertexPlot(VERTEX type, int x, int y) {
+      switch (type) {
+        case VERTEX_NONE:
+          break;
+        case VERTEX_CIR://頂点を丸くする
+          NefryDisplay.fillCircle(x, y, 2);
+          break;
+        case VERTEX_SQU://頂点を四角くする
+          NefryDisplay.fillRect(x - 2, y - 2, 4, 4);
+          break;
+      }
     }
 
   private:
-      int p[128][2];
+    //グラフ領域・ピッチ・更新時間
+    static int lpTime;
+    static int posX;
+    static int posY;
+    static int lenX;
+    static int lenY;
+    static int dpp;//点をプロットする間隔(1なら1ドットにつき1点、2なら2ドットにつき1点)
+    static int valueMIN;
+    static int valueMAX;
+    static int valueSIZE;
+    int *x;
+    int tCntMax; //
+    int tCnt;
+    int *t;
 
+    //描画するグラフの値・頂点などの情報
+    class graph {
+      public:
+        bool isSet;
+        bool isDispMax;
+        VERTEX type; //頂点のタイプ
+        int *v; //頂点の値
+        int *p; //プロットする頂点座標
+        int vMax[2];  //最大値,平行線のy座標
 
+        //コンストラクタ
+        graph() {
+          isSet = false;
+          isDispMax = false;
+        }
+        graph(int *_v, int *_p, VERTEX _type, bool _isDispMax) {
+          isSet = true;
+          isDispMax = _isDispMax;
+          type = _type;
+          v = _v;
+          p = _p;
+        }
 
+        void init() {
+          for (int i = 0; i < valueSIZE; i++) {
+            *(v + i) = 0;
+            *(p + i)  = lenY + posY;
+          }
+        }
+
+        void addData(int _v) {
+          vMax[0] = _v;  //最大値に仮設定
+          //前にずらす
+          for (int i = 0; i < valueSIZE - 1; i++) {
+            *(v + i) = *(v + i + 1);
+            *(p + i) = *(p + i + 1);
+            if (*(v + i) > vMax[0]) vMax[0] = *(v + i);
+          }
+          //最後尾に追加する
+          *(v + valueSIZE - 1) = _v;
+          *(p + valueSIZE - 1) = map(_v, valueMIN, valueMAX, lenY + posY, posY);
+          vMax[1] = map(vMax[0], valueMIN, valueMAX, lenY + posY, posY);
+        }
+    };
+    graph _g[GRAPH_CNT];
 };
-
 #endif
