2f785293194172e327d58a88a1d1746e72349aeb
Teppei Kuroda <niamugi@gmail.com>
2018-09-20 14:07:40 +0900

    加速度を描画する処理を追加

--- LissajousFigure/LissajousFigure.ino
+++ LissajousFigure/LissajousFigure.ino
@@ -1,3 +1,5 @@
+//リサージュ図形を描画
+//サンプルデータに加速度を使う。
 #include <Nefry.h>
 #include <NefryDisplay.h>
 #include <NefrySetting.h>
@@ -6,6 +8,17 @@ void setting() {
   Nefry.disableWifi();
 }
 NefrySetting nefrySetting(setting);
+
+#include "MPU6050_Manage.h"
+MPU6050_Manage mpu_main;
+
+//MPU6050の初期化時に使用するオフセット
+//CalibrationがOFFの時に適用される
+int CalOfs[4] = { -263, -36, -13, 1149}; //Gyro x,y,z, Accel z
+
+//MPU6050から取得するデータ
+int mpu6050_RealAccel[3];        //[x,y,z]
+int mpu6050_WorldAccel[3];       //[x,y,z]
 
 //切り替えボタン
 #define PIN_SW D8
@@ -26,7 +39,7 @@ float deg = 0;
 int graph_lissajous::valueSIZE;
 
 //n秒分プロットする
-#define PLOTTIME_US 500000
+#define PLOTTIME_US 1000000
 #define PLOT_SIZE (PLOTTIME_US / LOOPTIME_GRAPH)
 
 graph_lissajous grlissajous[2];
@@ -44,8 +57,10 @@ void dispGraphLissajous_init() {
   //x,y方向のドット数
   //値の最小値,最大値
   //プロット数
-  grlissajous[0] = graph_lissajous( 2, 2, 60, 60, -1, 1, PLOT_SIZE);
-  grlissajous[1] = graph_lissajous( 64, 2, 60, 60, -1, 1, PLOT_SIZE);
+//  grlissajous[0] = graph_lissajous( 2, 2, 60, 60, -1, 1, PLOT_SIZE);  //三角関数
+//  grlissajous[1] = graph_lissajous( 64, 2, 60, 60, -1, 1, PLOT_SIZE); //ランダム値
+  grlissajous[0] = graph_lissajous( 2, 2, 60, 60, -8192, 8192, PLOT_SIZE);
+  grlissajous[1] = graph_lissajous( 64, 2, 60, 60, -8192, 8192, PLOT_SIZE);
   grlissajous[0].setGraph(&p0[0][0]);
   grlissajous[1].setGraph(&p1[0][0]);
 }
@@ -80,6 +95,8 @@ void setup() {
 
   //サンプルデータ生成用乱数シード
   randomSeed(analogRead(0));
+
+  mpu_main.init(false, CalOfs);
 }
 
 void loop() {
@@ -90,10 +107,14 @@ void loop() {
 
   //データ・グラフ更新
   interval<LOOPTIME_GRAPH>::run([] {
+    mpu_main.updateValue();
+    mpu_main.Get_RealAccel(mpu6050_RealAccel);
+    mpu_main.Get_WorldAccel(mpu6050_WorldAccel);
     if (!isStopGraph) {
-      //-1～1を描画
-      grlissajous[0].addGraphData(cos((deg + 30) / (180 / PI)), cos(deg / (180 / PI)));
-      grlissajous[1].addGraphData(random(-50, 50) / (float)100, random(-100, 100) / (float)100);
+      //grlissajous[0].addGraphData(cos((deg + 30) / (180 / PI)), cos(deg / (180 / PI)));
+      //grlissajous[1].addGraphData(random(-50, 50) / (float)100, random(-100, 100) / (float)100);
+      grlissajous[0].addGraphData(mpu6050_WorldAccel[0],mpu6050_WorldAccel[1]);
+      grlissajous[1].addGraphData(mpu6050_WorldAccel[0],mpu6050_WorldAccel[2]);
       deg += 10;
     }
 
