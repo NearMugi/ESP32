f178f0fae2342e18158e134a2ab3615e34de9fc5
Teppei Kuroda <niamugi@gmail.com>
2018-12-31 16:59:35 +0900

    リフレッシュトークンからアクセストークンを取得 このサイトを参考(ほぼコピペ) https://github.com/jalmeroth/ESP8266-OAUTH2/blob/master/ESP8266-OAUTH2/ESP8266-OAUTH2.ino

--- GoogleAPIToken/RefreshToken/RefreshToken.ino
+++ GoogleAPIToken/RefreshToken/RefreshToken.ino
@@ -1,72 +1,34 @@
 #include <Nefry.h>
-#include <NefryDisplay.h>
-#include <HTTPClient.h>
+
+#include "googleAPI.h"
+googleAPI api;
 
 #define NEFRY_DATASTORE_REFRESH_TOKEN 5
 #define NEFRY_DATASTORE_CLIENT_ID 6
 #define NEFRY_DATASTORE_CLIENT_SECRET 7
 
-#define BODY_SIZE 206
-char body[BODY_SIZE];
-
-void setupAccessToken() {
-  Nefry.setStoreTitleStr("Refresh Token", NEFRY_DATASTORE_REFRESH_TOKEN);
-  Nefry.setStoreTitleStr("Client ID", NEFRY_DATASTORE_CLIENT_ID);
-  Nefry.setStoreTitleStr("Client Secret", NEFRY_DATASTORE_CLIENT_SECRET);
-
-  String _tmp = "";
-  _tmp += F("refresh_token=");
-  _tmp += Nefry.getStoreStr(NEFRY_DATASTORE_REFRESH_TOKEN);
-  _tmp += F("&");
-
-  _tmp += F("client_id=");
-  _tmp += Nefry.getStoreStr(NEFRY_DATASTORE_CLIENT_ID);
-  _tmp += F("&");
-
-  _tmp += F("client_secret=");
-  _tmp += Nefry.getStoreStr(NEFRY_DATASTORE_CLIENT_SECRET);
-  _tmp += F("&");
-  _tmp += F("grant_type=refresh_token");
-  _tmp.toCharArray(body, BODY_SIZE + 1);
-}
-
-String getAccessToken() {
-  String ret_Token = "";
-  Serial.println(F("++++++++++++++++++++\n[Get AccessToken]\n++++++++++++++++++++"));
-
-  HTTPClient http;
-  http.begin("https://www.googleapis.com/oauth2/v4/token");
-  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-
-  int httpCode = http.POST(body);
-  Serial.println(httpCode);
-  String result = "";
-  if (httpCode < 0) {
-    result = http.errorToString(httpCode);
-  } else {
-    Serial.println(http.getSize());
-    result = http.getString();
-  }
-
-  http.end();
-  Serial.println(result);
-
-  return ret_Token;
-}
+String refresh_token = "";
+String client_id = "";
+String client_secret = "";
+String accessToken = "";
 
 void setup() {
   Nefry.setProgramName("Get GoogleAPI AccessToken");
 
-  Nefry.enableSW();
+  Nefry.setStoreTitleStr("Refresh Token", NEFRY_DATASTORE_REFRESH_TOKEN);
+  Nefry.setStoreTitleStr("Client ID", NEFRY_DATASTORE_CLIENT_ID);
+  Nefry.setStoreTitleStr("Client Secret", NEFRY_DATASTORE_CLIENT_SECRET);
 
-  setupAccessToken();
+  refresh_token = Nefry.getStoreStr(NEFRY_DATASTORE_REFRESH_TOKEN);
+  client_id = Nefry.getStoreStr(NEFRY_DATASTORE_CLIENT_ID);
+  client_secret = Nefry.getStoreStr(NEFRY_DATASTORE_CLIENT_SECRET);
+
+  delay(5000);
+
+  accessToken = api.GetAccessToken(refresh_token, client_id, client_secret);
+  Serial.println(accessToken);
 
 }
 
 void loop() {
-  if (Nefry.readSW()) {
-    delay(1000);
-    getAccessToken();
-  }
-
 }
