a75722dcf1f1abb437c8537b62f2828d6a4cb0fd
Teppei Kuroda <niamugi@gmail.com>
2019-04-12 19:41:50 +0900

    M5Stackで作ったものをNefryBT側に反映

--- GoogleAPI/#createLibrary/googleAPI/CloudFunctions/CloudFunctions.ino
+++ GoogleAPI/#createLibrary/googleAPI/CloudFunctions/CloudFunctions.ino
@@ -1,56 +1,78 @@
 //CloudFunctionsを使う。
 
-//callCloudFunctions
+//+ callCloudFunctions_String
 //CloudFunctionsにPOSTリクエストする
-//引数：関数名,POSTデータ
-//返値：指定した関数からの返値
+//引数：ホスト,関数名,POSTデータ
+//返値：指定した関数からの返値(String)
 
-//getRuntimeConfig
+//+ callCloudFunctions_Json
+//基本callCloudFunctions_Stringと同じ。jsonデータを返す
+
+//+ getRuntimeConfig
 //RuntimeConfigのデータを取得する
-//引数：POSTするJSONデータ {"list" : ["CONFIGNAME1","CONFIGNAME2"]}
-//返値：[{"lbl":"CONFIGNAME","data":{"変数名1":"値","変数名2":"値"}}]
+//引数：list
+//返値：{"lbl":"CONFIGNAME","data":{"変数名1":"値","変数名2":"値"}}
+
+//+ getJsonValue
+//jsonからデータを取得する
+//引数：json, ターゲット
+//返値：ターゲットの値
 
 #include <Nefry.h>
+#include <ArduinoJson.h>
+
+//CloudFunctions
 #include "googleCloudFunctions.h"
 googleCloudFunctions cfs;
 
-#include <ArduinoJson.h>
+//https
+#define NEFRY_GCP_PROJECT 8
+String host = ""; //"[プロジェクト名].cloudfunctions.net"をNefryのデータストアから取得する
+String apikey = "";
+
+//RuntimeConfigのデータを取得する
+String getRuntimeConfigData(String _list, String _name)
+{
+  //RuntimeConfigのデータを取得する
+  String ret = cfs.getRuntimeConfig(host, _list);
+
+  //取得したjsonデータから欲しい情報を取得する
+  return cfs.getJsonValue(ret, _name);
+}
 
 void setup()
 {
+  //https
+  Nefry.setStoreTitleStr("GCP Project", NEFRY_GCP_PROJECT);
+  host = Nefry.getStoreStr(NEFRY_GCP_PROJECT);
+
+  //CloudFunctions
   cfs.InitAPI();
+  apikey = getRuntimeConfigData("googleAPI", "GPSApiKey");
 
-  //RuntimeConfigのデータを取得する
-  Serial.println("\n++++++++++++++++++++++\n[RuntimeConfig]\n++++++++++++++++++++++\n");
-  String ret = cfs.getRuntimeConfig("googleAPI");
-
-  //取得したjsonデータから欲しい情報を取得する
-  String GPSApiKey = cfs.getJsonValue(ret, "GPSApiKey");
-
-  //GPSMap_getMapBinaryを使う
-  Serial.println("\n++++++++++++++++++++++\n[GPSMap_getMapBinary]\n++++++++++++++++++++++\n");
+  //CloudFunctionsを使う例(GPSMap_getMapBinary)
   String _axis = "35.68035,139.7673229";
   String postData = "";
-  postData += "{\"key\" : \"" + GPSApiKey + "\",";
+  postData += "{\"key\" : \"" + apikey + "\",";
   postData += "\"axis\" : \"" + _axis + "\",";
   postData += "\"color\" : \"true\",";
-  postData += "\"trim\" : \"topright\"";
+  postData += "\"trim\" : \"1\"";
   postData += "}";
   Serial.println(postData);
 
-  String retBytes = cfs.callCloudFunctions("GPSMap_getMapBinary", postData);
-  uint8_t imgBytes[retBytes.length() + 1];
-  retBytes.getBytes(imgBytes, sizeof(imgBytes));
-
-  Serial.print("retBytes Size: ");
-  Serial.println(retBytes.length());
-  Serial.print("imgBytes Size: ");
-  Serial.println(sizeof(imgBytes));
-  for (int i = 0; i < sizeof(imgBytes) - 1; i++)
+  String retImage = cfs.callCloudFunctions_String(host, "GPSMap_getMapBinary", postData);
+  int len = retImage.length();
+  Serial.print(F("[Get Data] Size : "));
+  Serial.println(len);
+  Serial.println(retImage);
+  uint8_t bytes[len];
+  for (int i = 0; i < len; i++)
   {
-    //Serial.print(String(imgBytes[i], HEX));
-    //Serial.print(" ");
+    bytes[i] = retImage[i];
+    Serial.print(String(bytes[i], HEX));
+    Serial.print(" ");
   }
+  Serial.println("");
 }
 
 void loop()
--- GoogleAPI/#createLibrary/googleAPI/CloudFunctions/googleCloudFunctions.h
+++ GoogleAPI/#createLibrary/googleAPI/CloudFunctions/googleCloudFunctions.h
@@ -3,111 +3,159 @@
 #include <ArduinoJson.h>
 #include <WiFiClientSecure.h>
 
-#define NEFRY_GCP_PROJECT 8
+class googleCloudFunctions
+{
+public:
+  String basePostHeader;
 
-class googleCloudFunctions {
-  public:
-    String basePostHeader;
+  //リクエストするときのヘッダーを設定する
+  void InitAPI()
+  {
+    basePostHeader = "";
+    basePostHeader += ("POST @function HTTP/1.1\r\n");
+    basePostHeader += ("Host: @host:@httpsPort\r\n");
+    basePostHeader += ("Connection: close\r\n");
+    basePostHeader += ("Content-Type: application/json;charset=utf-8\r\n");
+  }
 
-    void InitAPI() {
-      Nefry.setStoreTitleStr("GCP Project", NEFRY_GCP_PROJECT);
-      //リクエストするときのヘッダーを設定する
-      basePostHeader = "";
-      basePostHeader += ("POST @function HTTP/1.1\r\n");
-      basePostHeader += ("Host: @host:@httpsPort\r\n");
-      basePostHeader += ("Connection: close\r\n");
-      basePostHeader += ("Content-Type: application/json;charset=utf-8\r\n");
+  //RuntimeConfigからデータを取得する
+  String getRuntimeConfig(String _host, String _configName)
+  {
+    String postData = "{\"list\" : [\"" + _configName + "\"]}";
+    return callCloudFunctions_Json(_host, "GetRuntimeConfig", postData);
+  }
+
+  //関数名とPOSTリクエストを指定して実行、レスポンス(JSON)を返す
+  String callCloudFunctions_Json(String _host, String _funcName, String _postData)
+  {
+    char *res;
+    res = (char *)malloc(2000 * sizeof(char));
+    int len = postCloudFunctions(_host, _funcName, _postData, res);
+
+    String ret = "";
+    for (int i = 0; i < len; i++)
+    {
+      ret += res[i];
     }
+    //最初と最後の"[]"を外す
+    ret.setCharAt(0, ' ');
+    ret.setCharAt(ret.length() - 1, ' ');
+    ret.trim();
+#ifdef DEBUG
+    Serial.print(F("[Get Data] Size : "));
+    Serial.println(ret.length());
+    Serial.println(ret);
+#endif
+    free(res);
+    return ret;
+  }
 
-    //RuntimeConfigからデータを取得する
-    String getRuntimeConfig(String _configName) {
-      String postData = "{\"list\" : [\"" + _configName + "\"]}";
-      return callCloudFunctions("GetRuntimeConfig", postData);
+  //関数名とPOSTリクエストを指定して実行、レスポンス(String)を返す
+  String callCloudFunctions_String(String _host, String _funcName, String _postData)
+  {
+    char *res;
+    res = (char *)malloc(6000 * sizeof(char));
+    int len = postCloudFunctions(_host, _funcName, _postData, res);
+
+    //Serial.println(len);
+    String ret = "";
+    for (int i = 0; i < len; i++)
+    {
+      ret += res[i];
+      //Serial.print(String(res[i], HEX));
+      //Serial.print(" ");
     }
+    //Serial.println("");
 
-    //関数名とPOSTリクエストを指定して実行、レスポンスを返す
-    String callCloudFunctions(String _funcName, String _postData) {
-      String _tmp = Nefry.getStoreStr(NEFRY_GCP_PROJECT);
-      const char* host = _tmp.c_str();
-      const int httpsPort = 443;
-      String function = "/" + _funcName;
-      String postData = _postData;
+    free(res);
+    return ret;
+  }
 
-      String postHeader = basePostHeader;
-      postHeader.replace("@function", function);
-      postHeader.replace("@host", String(host));
-      postHeader.replace("@httpsPort", String(httpsPort));
-      postHeader = postHeader + "Content-Length: " + postData.length() + "\r\n" + "\r\n" + postData + "\r\n";
+  //jsonデータから欲しい情報を取得する
+  String getJsonValue(String _target, String _member)
+  {
+    StaticJsonDocument<700> doc;
+    char json[_target.length() + 1];
+    int size = sizeof(json);
+    _target.toCharArray(json, size);
 
-      String ret = postRequest(host, httpsPort, postHeader);
-      //最初と最後の"[]"を外す
-      ret.setCharAt(0, ' ');
-      ret.setCharAt(ret.length() - 1, ' ');
-      ret.trim();
-      return ret;
+    DeserializationError error = deserializeJson(doc, json);
+    if (error)
+    {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.c_str());
+      return "";
     }
+    const char *ret = doc[_member];
+    return ret;
+  }
 
-    //jsonデータから欲しい情報を取得する
-    String getJsonValue(String _target, String _member) {
-      StaticJsonDocument<700> doc;
-      char json[_target.length() + 1];
-      int size = sizeof(json);
-      _target.toCharArray(json, size);
+private:
+  //CloudFunctionsにポストする
+  //取得したサイズを返す
+  int postCloudFunctions(String _host, String _funcName, String _postData, char *res)
+  {
+    const char *host = _host.c_str();
+    const int httpsPort = 443;
+    String function = "/" + _funcName;
+    String postData = _postData;
 
-      DeserializationError error = deserializeJson(doc, json);
-      if (error) {
-        Serial.print(F("deserializeJson() failed: "));
-        Serial.println(error.c_str());
-        return "";
+    String postHeader = basePostHeader;
+    postHeader.replace("@function", function);
+    postHeader.replace("@host", String(host));
+    postHeader.replace("@httpsPort", String(httpsPort));
+    postHeader = postHeader + "Content-Length: " + postData.length() + "\r\n" + "\r\n" + postData + "\r\n";
+
+    // Use WiFiClientSecure class to create TLS connection
+    WiFiClientSecure client;
+#ifdef DEBUG
+    Serial.print(F("Connecting to: "));
+    Serial.println(host);
+#endif
+
+    if (!client.connect(host, httpsPort))
+    {
+      Serial.println(F("connection failed"));
+      return 0;
+    }
+#ifdef DEBUG
+    Serial.println(F("certificate matches"));
+    Serial.println(F("[post]"));
+    Serial.println(postHeader);
+#endif
+
+    client.print(postHeader);
+    delay(10);
+
+    while (client.connected())
+    {
+      String line = client.readStringUntil('\n');
+      if (line == "\r")
+      {
+#ifdef DEBUG
+        Serial.println(F("headers received"));
+#endif
+        break;
       }
-      const char* ret = doc[_member];
-      return ret;
     }
 
-  private:
-    //サーバーにデータをポストする
-    String postRequest(const char* server, int port, String header) {
-
-      String result = "";
-
-      // Use WiFiClientSecure class to create TLS connection
-      WiFiClientSecure client;
-      Serial.print("Connecting to: "); Serial.println(server);
-
-      if (!client.connect(server, port)) {
-        Serial.println("connection failed");
-        return result;
-      }
-      Serial.println("certificate matches");
-
-      Serial.println("[post]");
-      Serial.println(header);
-      client.print(header);
-      delay(10);
-
-      if (client.connected()) {
-        if (client.find("HTTP/1.1 ")) {
-          String status_code = client.readStringUntil('\r');
-          Serial.print("Status code: "); Serial.println(status_code);
-          if (status_code != "200 OK") {
-            Serial.println("There was an error");
-          }
-        }
-        if (client.find("\r\n\r\n")) {
-          Serial.println(F("[Read Data]"));
-        }
-
-        result = "";
-        while (client.available()) {
-          result += client.readStringUntil('\r');
-        }
-      }
-
-      Serial.print("ReceiveData Size:");Serial.println(result.length());
-      Serial.println(result);
-      Serial.println("closing connection");
-      return result;
+    delay(1000);
+    int len = 0;
+    while (client.available())
+    {
+      char c = client.read();
+      *(res++) = c;
+      len++;
+      //Serial.write(c);
+      //Serial.print(String(c,HEX));
+      //Serial.print(" ");
     }
+#ifdef DEBUG
+    Serial.println(F("\nclient stop"));
+#endif
+    client.stop();
 
+    return len;
+  }
 };
 #endif
