e60df873afe26a9dc59871819216f56753b0f2ff
Teppei Kuroda <niamugi@gmail.com>
2019-07-29 14:53:39 +0900

    MQTT追加。動作確認前。

--- NefryBT/AMG8833/AMG8833.ino
+++ NefryBT/AMG8833/AMG8833.ino
@@ -1,6 +1,9 @@
 #include <Nefry.h>
 #include <NefryDisplay.h>
 #include <Adafruit_AMG88xx.h>
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
 
 #include <NefrySetting.h>
 void setting() {
@@ -9,16 +12,66 @@ void setting() {
 }
 NefrySetting nefrySetting(setting);
 
+//date
+#include <time.h>
+#define JST     3600*9
+
+//MQTT
+#define NEFRY_DATASTORE_BEEBOTTE 1
+#define BBT "mqtt.beebotte.com"
+#define QoS 0
+String bbt_token;
+#define Channel "Cat"
+#define Res "AMG8833"
+char topic[64];
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 const int pixel_array_size = 8*8;
 float pixels[pixel_array_size];
+const unsigned int dataSize = 6 * pixel_array_size;
 
 Adafruit_AMG88xx amg;
 bool status;
+
+void mqttPublish(String _data){    
+    // Create a random client ID
+    String clientId = "ESP32Client-";
+    clientId += String(random(0xffff), HEX);
+
+    //NefryのDataStoreに書き込んだToken(String)を(const char*)に変換
+    bbt_token = "token:";
+    bbt_token += Nefry.getStoreStr(NEFRY_DATASTORE_BEEBOTTE);
+    const char* tmp = bbt_token.c_str();
+    // Attempt to connect
+    if (client.connect(clientId.c_str(), tmp, "")) {
+        time_t  t = time(NULL);
+        char buffer[dataSize];
+        StaticJsonBuffer<dataSize> jsonOutBuffer;
+        JsonObject& root = jsonOutBuffer.createObject();
+        root["AMG8833"] = _data;
+        root["ispublic"] = true;
+        root["ts"] = t;
+
+        // Now print the JSON into a char buffer
+        root.printTo(buffer, sizeof(buffer));
+
+        // Now publish the char buffer to Beebotte
+        client.publish(topic, buffer, QoS);
+    }    
+}
 
 void setup() {
 
     NefryDisplay.begin();
     NefryDisplay.setAutoScrollFlg(true); //自動スクロールを有効
+  
+    //date
+    configTime( JST, 0, "ntp.nict.jp", "ntp.jst.mfeed.ad.jp");
+    
+    Nefry.setStoreTitle("MQTT_Token", NEFRY_DATASTORE_BEEBOTTE); 
+    client.setServer(BBT, 1883);
+    sprintf(topic, "%s/%s", Channel, Res);
 
     NefryDisplay.clear();
     NefryDisplay.display();
@@ -40,5 +93,7 @@ void loop() {
         t += ",";
     }
     Serial.println(t);
+    mqttPublish(t);
+
     delay(100);
 }
