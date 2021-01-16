625145462be11928d5b13f1901b942c68cfad0ec
Teppei Kuroda <niamugi@gmail.com>
2021-01-16 16:03:53 +0900

    アメダスデータを配列に保存

--- M5Atom/accessJma/amedas.cpp
+++ M5Atom/accessJma/amedas.cpp
@@ -1,23 +1,12 @@
 #include "amedas.h"
 
-bool amedas::unitAmedasData::set(float _t, float _r, float _w, float _su, float _sn, float _h, float _p)
-{
-    temperature = _t;
-    rain = _r;
-    wind = _w;
-    sun = _su;
-    snow = _sn;
-    humidity = _h;
-    pressure = _p;
-    return true;
-}
-
 bool amedas::getData()
 {
+    String url = "http://www.jma.go.jp/jp/amedas_h/today-" + posID + ".html";
     String payload = "";
     HTTPClient http;
 
-    if (http.begin("http://www.jma.go.jp/jp/amedas_h/today-44132.html"))
+    if (http.begin(url))
     {
         int httpCode = http.GET();
         if (httpCode > 0)
@@ -50,8 +39,85 @@ bool amedas::getData()
     tblData.replace("</td>\n<td class=\"block middle\">", ",");
     tblData.replace("</tr>\n<tr>\n<td class=\"time left\">", "");
     tblData.replace("</td>", "");
-    Serial.println(tblData);
+    tblData.replace("</tr>", "");
+    tblData.replace("</table>", "");
+    tblData.replace("</div>", "");
+    //Serial.println(tblData);
 
-    //
-    //std::fill(amedasData.begin(), amedasData.end(), 24);
+    // 1～24時のデータ(1,6.9,0.0,西北西,2.1,,0,77,1017.4)を保存
+    Serial.println("\nGet Data");
+    String tmpSplitData = "";
+    std::vector<String> tmpTblLineData;
+    for (const char &c : tblData)
+    {
+        if (c == '\n')
+        {
+            if (std::isdigit(tmpSplitData[0]) != 0)
+            {
+                Serial.println(tmpSplitData);
+                tmpTblLineData.push_back(tmpSplitData);
+            }
+            tmpSplitData = "";
+        }
+        else
+        {
+            tmpSplitData += c;
+        }
+    }
+
+    // カンマで区切ってデータを保存
+    for (const String &t : tmpTblLineData)
+    {
+        tmpSplitData = "";
+        std::vector<String> tmpTblData;
+        for (const char &c : t)
+        {
+            if (c == ',')
+            {
+                //Serial.println(tmpSplitData);
+                tmpTblData.push_back(tmpSplitData);
+                tmpSplitData = "";
+            }
+            else
+            {
+                tmpSplitData += c;
+            }
+        }
+        if (std::isdigit(tmpSplitData[0]) != 0)
+        {
+            //Serial.println(tmpSplitData);
+            tmpTblData.push_back(tmpSplitData);
+        }
+
+        // 先頭のデータを削除
+        tmpTblData.erase(tmpTblData.begin());
+
+        // 先頭の値(気温)が数値の場合、データを保存する
+        if (tmpTblData[0].length() > 0)
+        {
+            _tblData.emplace_back(
+                unitAmedasData(
+                    tmpTblData[0].toFloat(),
+                    tmpTblData[1].toFloat(),
+                    tmpTblData[2],
+                    tmpTblData[3].toFloat(),
+                    tmpTblData[4].toFloat(),
+                    tmpTblData[5].toFloat(),
+                    tmpTblData[6].toFloat(),
+                    tmpTblData[7].toFloat()));
+        }
+    }
+
+    Serial.println("\nTable Size");
+    Serial.println(_tblData.size());
+    _lastData = _tblData[_tblData.size() - 1];
+    Serial.println("\nLast Data");
+    Serial.println(_lastData.getTemperature());
+    Serial.println(_lastData.getRain());
+    Serial.println(_lastData.getWindDirection());
+    Serial.println(_lastData.getWind());
+    Serial.println(_lastData.getSun());
+    Serial.println(_lastData.getSnow());
+    Serial.println(_lastData.getHumidity());
+    Serial.println(_lastData.getPressure());
 }
--- M5Atom/accessJma/amedas.h
+++ M5Atom/accessJma/amedas.h
@@ -10,18 +10,34 @@ class amedas
     public:
         unitAmedasData() : temperature(0.0),
                            rain(0.0),
+                           windDirection(""),
                            wind(0.0),
                            sun(0.0),
                            snow(0.0),
                            humidity(0.0),
                            pressure(0.0){};
+        unitAmedasData(float _t, float _r, String _wd, float _w, float _su, float _sn, float _h, float _p) : temperature(_t),
+                                                                                                             rain(_r),
+                                                                                                             windDirection(_wd),
+                                                                                                             wind(_w),
+                                                                                                             sun(_su),
+                                                                                                             snow(_sn),
+                                                                                                             humidity(_h),
+                                                                                                             pressure(_p){};
         virtual ~unitAmedasData(){};
-
-        bool set(float _t, float _r, float _w, float _su, float _sn, float _h, float _p);
+        float getTemperature() const { return temperature; };
+        float getRain() const { return rain; };
+        String getWindDirection() const { return windDirection; };
+        float getWind() const { return wind; };
+        float getSun() const { return sun; };
+        float getSnow() const { return snow; };
+        float getHumidity() const { return humidity; };
+        float getPressure() const { return pressure; };
 
     private:
         float temperature;
         float rain;
+        String windDirection;
         float wind;
         float sun;
         float snow;
@@ -30,14 +46,15 @@ class amedas
     };
 
 public:
-    amedas() : _tblData(), posID("44132"){};
-    amedas(String _id) : _tblData(), posID(_id){};
-    amedas(int _id) : _tblData(), posID(String(_id)){};
+    amedas() : _tblData(), _lastData(), posID("00000"){};
+    amedas(String _id) : _tblData(), _lastData(), posID(_id){};
+    amedas(int _id) : _tblData(), _lastData(), posID(String(_id)){};
     ~amedas(){};
     bool getData();
 
 private:
     std::vector<unitAmedasData> _tblData;
+    unitAmedasData _lastData;
     String posID;
 };
 #endif
