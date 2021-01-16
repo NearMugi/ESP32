8676fd4c78eadfc1dc3923d6ae7c871829a0e4ce
Teppei Kuroda <niamugi@gmail.com>
2021-01-16 16:16:16 +0900

    不要なインクルードファイルを削除

--- M5Atom/accessJma/accessJma.ino
+++ M5Atom/accessJma/accessJma.ino
@@ -2,7 +2,6 @@
 
 #include <WiFi.h>
 #include <WiFiMulti.h>
-#include <ArduinoJson.h>
 
 #include "amedas.h"
 
@@ -14,7 +13,7 @@ void setup()
 {
     Serial.begin(115200);
     WiFi.mode(WIFI_STA);
-    WiFiMulti.addAP("Buffalo-G-36F0", "pinkpanther48");
+    WiFiMulti.addAP("Buffalo-G-36F0", "[pass]");
 
     // wait for WiFi connection
     Serial.print("Waiting for WiFi to connect...");
